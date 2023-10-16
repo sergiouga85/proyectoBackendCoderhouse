5b2e464d79a5b89931820a5228f4d6dd6ede463b
Sergio Ugarte <sergiouga85@gmail.com>
2023-10-16 16:35:37 -0300

    inserccion ruta de la imagen

--- ProyectoBackend.js
+++ ProyectoBackend.js
@@ -2,11 +2,12 @@ class Product{
 
     #stock
 
-    constructor({id,title,description,price,stock}){
+    constructor({id,title,description,price,thumbnail,stock}){
         this.id = id
         this.title= title
         this.description=description
         this.price=price
+        this.thumbnail=thumbnail
         this.setStock(stock)
     }
 
@@ -27,6 +28,7 @@ class Product{
             title:this.title,
             description:this.description,
             price:this.price,
+            thumbnail:this.thumbnail,
             stock:this.#stock  
         }
     }
@@ -45,9 +47,9 @@ class ProductsManager{
         return ++ProductsManager.#ultimoId
     }
 
-    addProducts({title,description,price,stock}){
+    addProducts({title,description,price,thumbnail,stock}){
         const id= ProductsManager.#generarNuevoId()
-        const product=new Product({id, title,description,price,stock})
+        const product=new Product({id, title,description,price,thumbnail,stock})
         this.#products.push(product)
         return product
     }
@@ -66,10 +68,10 @@ class ProductsManager{
 const pm= new ProductsManager()
 
 
-const p1= pm.addProducts({title:'Arroz', description:'legunbre', price: 150, stock: 10})
-const p2= pm.addProducts({title: 'Arbejas', description:'legunbre', price: 200, stock: 5})
-const p3= pm.addProducts({title:'Aceite', description:'aceite de cocina', price: 600, stock: 15})
-const p4= pm.addProducts({title:'Sal', description:'condimento', price: 300, stock: 25})
+const p1= pm.addProducts({title:'Arroz', description:'legunbre', thumbnail:'https://www.mayoristanet.com/media/catalog/product/cache/7c7e7e8fca0426f106cb3e3371a80f9c/A/0/A08356.jpg', price: 150, stock: 10})
+const p2= pm.addProducts({title: 'Arbejas', description:'legunbre', thumbnail: 'https://http2.mlstatic.com/D_NQ_NP_2X_933337-MLA51179217372_082022-F.webp', price: 200, stock: 5})
+const p3= pm.addProducts({title:'Aceite', description:'aceite de cocina', thumbnail: 'https://http2.mlstatic.com/D_NQ_NP_688087-MLA49934225598_052022-O.webp', price: 600, stock: 15})
+const p4= pm.addProducts({title:'Sal', description:'condimento', thumbnail: 'https://http2.mlstatic.com/D_NQ_NP_716583-MLA43119567167_082020-O.webp', price: 300, stock: 25})
 
 
 console.log(pm.getProducts())
