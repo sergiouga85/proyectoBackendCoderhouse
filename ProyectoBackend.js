5e8c1b2e6e4beeb105ab8da8dfee026374a9e87e
Sergio Ugarte <sergiouga85@gmail.com>
2023-10-16 18:00:37 -0300

    primera pre entrega modificaciones

--- ProyectoBackend.js
+++ ProyectoBackend.js
@@ -2,12 +2,13 @@ class Product{
 
     #stock
 
-    constructor({id,title,description,price,thumbnail,stock}){
+    constructor({id,title,description,price,thumbnail,code,stock}){
         this.id = id
         this.title= title
         this.description=description
         this.price=price
         this.thumbnail=thumbnail
+        this.code=code
         this.setStock(stock)
     }
 
@@ -29,6 +30,7 @@ class Product{
             description:this.description,
             price:this.price,
             thumbnail:this.thumbnail,
+            code:this.code,
             stock:this.#stock  
         }
     }
@@ -47,9 +49,15 @@ class ProductsManager{
         return ++ProductsManager.#ultimoId
     }
 
-    addProducts({title,description,price,thumbnail,stock}){
+    addProducts({title,description,price,thumbnail,code,stock}){
+
+        if(!title || !description || !price || !thumbnail || !code || !stock){
+            throw new Error('Todos los campos son obligatorios');
+        }
+        const productCode= this.#products.find((p)=>p.code===code)
+        if(productCode) throw new Error('Otro producto ya fue agregado con ese codigo')
         const id= ProductsManager.#generarNuevoId()
-        const product=new Product({id, title,description,price,thumbnail,stock})
+        const product=new Product({id,title,description,price,thumbnail,code,stock})
         this.#products.push(product)
         return product
     }
@@ -67,28 +75,21 @@ class ProductsManager{
 
 const pm= new ProductsManager()
 
-
-const p1= pm.addProducts({title:'Arroz', description:'legunbre', thumbnail:'https://www.mayoristanet.com/media/catalog/product/cache/7c7e7e8fca0426f106cb3e3371a80f9c/A/0/A08356.jpg', price: 150, stock: 10})
-const p2= pm.addProducts({title: 'Arbejas', description:'legunbre', thumbnail: 'https://http2.mlstatic.com/D_NQ_NP_2X_933337-MLA51179217372_082022-F.webp', price: 200, stock: 5})
-const p3= pm.addProducts({title:'Aceite', description:'aceite de cocina', thumbnail: 'https://http2.mlstatic.com/D_NQ_NP_688087-MLA49934225598_052022-O.webp', price: 600, stock: 15})
-const p4= pm.addProducts({title:'Sal', description:'condimento', thumbnail: 'https://http2.mlstatic.com/D_NQ_NP_716583-MLA43119567167_082020-O.webp', price: 300, stock: 25})
-
-
-console.log(pm.getProducts())
-
-console.log(pm.getProducts().map((p)=>p.asPOJO()))
-
-
 try{
-    const producto1= p1.setStock(-1)
-    console.log(producto1)
+    const p1= pm.addProducts({title:'Arroz', description:'legunbre', price: 150, thumbnail:'sin imagen', code: 'ACB1',   stock: 10})
+    const p2= pm.addProducts({title: 'Arbejas', description:'legunbre', price: 200, thumbnail: 'sin imagen', code: 'ACB2', stock: 5})
+    const p3= pm.addProducts({title:'Aceite', description:'aceite de cocina', price: 600, thumbnail:'sin imagen' , code: 'ACB3', stock: 15})
+    const p4= pm.addProducts({title:'Sal', description:'condimento', price: 300,  thumbnail: 'sin imagen', code: 'ACB4', stock: 25})
+    
+    console.log(pm.getProducts().map((p)=>p.asPOJO()))
+   
 }
 catch(error){
     console.log(error.message)
 }
 
 try{
-    const productoId= pm.getProductsById(6).asPOJO()
+    const productoId= pm.getProductsById(1).asPOJO()
     console.log(productoId)
 }
 catch(error){
